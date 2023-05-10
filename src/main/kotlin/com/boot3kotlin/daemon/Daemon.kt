c68b7076f321790bb7f2d502bdf00018b0befe78
Toto Hyojun Tak <thj0309@hotmail.com>
2023-05-10 13:39:22 +0900

    modi

--- src/main/kotlin/com/boot3kotlin/daemon/Daemon.kt
+++ src/main/kotlin/com/boot3kotlin/daemon/Daemon.kt
@@ -6,7 +6,7 @@ import kotlin.concurrent.thread
 
 fun main(args: Array<String>){
     //    thread(start = true, isDaemon = false) {
-    thread(start = true, isDaemon = false) {
+    thread(start = true, isDaemon = false) { // isDaemon이 false 이면 종료, true 이면 지속
         while (true) {
             Thread.sleep(1000L)
             println(ZonedDateTime.now(ZoneId.of("Asia/Seoul")))
